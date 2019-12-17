693f52cce1742d5da4f2896a3013814e0dd2b719
bluesuperworking <wayne.ferrao@bell.ca>
2019-12-17 17:45:22 -0500

    Integrated express & refactored firebase functions

---
+++ functions/dbschema.js
@@ -0,0 +1,11 @@
+let db = {
+    shouts: [
+        {
+            userHandle: 'user',
+            body: 'this is the shout body',
+            createdAt: '2019-12-17T22:22:47.480Z',
+            likeCount: 5,
+            commentCount: 3,
+        }
+    ]
+}
--- functions/index.js
+++ functions/index.js
@@ -2,44 +2,52 @@ const functions = require('firebase-functions');
 let admin = require("firebase-admin");
 let serviceAccount = require("./serviceAccountKey.json");
 
-
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: "https://monkeytime-9a89a.firebaseio.com"
 });
-  
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-exports.helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello Heroes!!");
-});
 
-exports.getShouts = functions.https.onRequest((req, res) => {
+const express = require('express');
+const app = express();
+
+//  Express allows us to use the same endpoint name ('shouts'), but handle 2 endpoints; GET, POST etc.
+// Without express, you'd have to check whether we're doing POST or GET and responde accordingly.
+// The first param is name of route and 2nd is the handler
+app.get('/shouts',(req, res) => {
     //generally you run db.collection('collectionname')
     //in this case, the db is admin.firestore
     //.get returns a promise which holds a querySnapshot, which contains an array of documents
-    admin.firestore().collection('shouts').get()
+    admin
+        .firestore()
+        .collection('shouts')
+        .orderBy('createdAt','desc')
+        .get()
         .then((data) => {
             let posts = [];
             data.forEach((doc) => {//doc is just a reference. Use .data() returns data inside document
-                posts.push(doc.data());
+                posts.push({
+                    shoutId: doc.id,
+                    body: doc.data().body,
+                    userHandle: doc.data().userHandle,
+                    createdAt: doc.data().createdAt
+                }); 
             });
             return res.json(posts);
-        })//This returns a Promise so it's good practice to put a catch for any potential errors.
-        .catch((err) => console.error(err));
-});
+        })//This returns a Promise so it's good practice  to put a catch for any potential errors.
+        .catch((err) => console.error(err)); 
+}) 
 
-exports.createShout = functions.https.onRequest((req, res) => {
+// This funciton also accesses the same endpoint name, but with a POST request.
+app.post('/shout', (req, res) => {
     if(req.method !== 'POST'){
         return res.status(400).json({error:'Method not allowed'})
     }
     const newShout = {
         body: req.body.body,
         userHandle: req.body.userHandle,
-        createdAt: admin.firestore.Timestamp.fromDate(new Date())
+        createdAt: new Date().toISOString()
     } 
-    admin.firestore()
+    admin.firestore() 
         .collection('shouts')
         .add(newShout)
         .then(doc =>{
@@ -49,3 +57,5 @@ exports.createShout = functions.https.onRequest((req, res) => {
         console.error(err); 
     })
 });
+
+exports.api = functions.https.onRequest(app); //This automatically turns into multiple routes
