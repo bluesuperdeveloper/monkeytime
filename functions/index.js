2036da41b38b813c9b7270efef5a5b3f9c1d2d3f
bluesuperworking <wayne.ferrao93@gmail.com>
2019-12-19 21:35:01 -0500

    Added middleware function to validate bearer token

--- functions/index.js
+++ functions/index.js
@@ -10,7 +10,6 @@ admin.initializeApp({
 });
 
 let firebaseConfig = require('./firebaseConfig.json');
-
 const firebase = require('firebase');
 firebase.initializeApp(firebaseConfig);
 
@@ -40,9 +39,43 @@ app.get('/shouts', (req, res) => {
         .catch((err) => console.error(err));
 });
 
+const FBAuth = (req, res, next) => {
+    let idToken; // initialize ID Token
+    // Checks if the authorization header exists and if it starts with Bearer
+    if (req.headers.authorization && req.headers.authorization.startsWith('Bearer ')) {
+        // Extract the id token.
+        idToken = req.headers.authorization.split('Bearer ')[1];
+    } else {
+        console.error('No token found');
+        return res.status(403).json({error: 'Unauthorized'});
+    }
+    // Need to verify that token was issued by our application and not somewhere else
+    admin.auth().verifyIdToken(idToken)
+        .then((decodedToken) => { // holds user data inside token
+            req.user = decodedToken;
+            return db.collection('users')
+                .where('userId', '==', req.user.uid)
+                .limit(1) // limits results to 1 document
+                .get();
+        })
+        .then((data) => {
+            req.user.handle = data.docs[0].data().handle;
+            return next();
+        })
+        .catch((err) => { // token invalid, blacklisted
+            console.error('Error while verifying token', err);
+            return res.status(403).json(err);
+        });
+};
+
 // This function also accesses the same endpoint name, but with a POST request.
-app.post('/shout', (req, res) => {
+// Post 1 shout
+app.post('/shout', FBAuth, (req, res) => {
     // Assigns data in request body to newShout
+    if (req.body.body.trim() === '') {
+        return res.status(400).json({body: 'Body must not be empty'});
+    }
+
     const newShout = {
         body: req.body.body,
         userHandle: req.body.userHandle,
